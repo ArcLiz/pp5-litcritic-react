7b3152219a1b6481999cef4319d928f562fbf799
ArcLiz <gyllingstenlizah@gmail.com>
2024-06-26 16:59:39 +0000

    Add styling and error handling to signin form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,20 +1,23 @@
-import React, {useState} from "react";
-import {useHistory} from "react-router-dom";
-import {Form, Button, Alert} from "react-bootstrap";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Form, Button, Alert, Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
-import {useSetCurrentUser} from "../../contexts/CurrentUserContext";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
 import { setTokenTimestamp } from "../../utils/utils";
+import styles from "../../styles/SignInForm.module.css";
 
 const SignInForm = () => {
   const setCurrentUser = useSetCurrentUser();
-  useRedirect('loggedIn');
+  useRedirect("loggedIn");
+
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
   });
+
   const [errors, setErrors] = useState({});
-  const {username, password} = signInData;
+  const { username, password } = signInData;
   const history = useHistory();
 
   const handleChange = (event) => {
@@ -27,8 +30,7 @@ const SignInForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const {data} = await axios.post("/dj-rest-auth/login/", signInData);
-      await axios.post("/dj-rest-auth/login/", signInData);
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
       setTokenTimestamp(data);
       history.push("/");
@@ -38,43 +40,70 @@ const SignInForm = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="text" name="username" value={username} onChange={handleChange} placeholder="Enter username" />
-        {errors.username && (
-          <Alert variant="danger" className="mt-1">
-            {errors.username.map((error, idx) => (
-              <div key={idx}>{error}</div>
-            ))}
-          </Alert>
-        )}
-      </Form.Group>
+    <Container>
+      <Row className="justify-content-center mt-2">
+        <Col xs={12} md={8} lg={6} className={styles.mainContainer}>
+          <h1 className="mb-4 text-center">Welcome back!</h1>
+          <p className="text-muted small">
+          "A word after a word after a word is power." – Margaret Atwood
+          </p>
+          <hr />
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Control
+                type="text"
+                name="username"
+                value={username}
+                onChange={handleChange}
+                placeholder="Enter username"
+              />
+              {errors.username && (
+                <Alert variant="danger" className="mt-2">
+                  {errors.username.map((error, idx) => (
+                    <div key={idx}>{error}</div>
+                  ))}
+                </Alert>
+              )}
+            </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" value={password} onChange={handleChange} placeholder="Password" />
-        {errors.password && (
-          <Alert variant="danger" className="mt-1">
-            {errors.password.map((error, idx) => (
-              <div key={idx}>{error}</div>
-            ))}
-          </Alert>
-        )}
-      </Form.Group>
+            <Form.Group controlId="password" className="my-2">
+              <Form.Control
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                placeholder="Enter password"
+              />
+              {errors.password && (
+                <Alert variant="danger" className="mt-2">
+                  {errors.password.map((error, idx) => (
+                    <div key={idx}>{error}</div>
+                  ))}
+                </Alert>
+              )}
+            </Form.Group>
 
-      <Button variant="success" type="submit">
-        Sign In
-      </Button>
+            <Button variant="primary" type="submit" className={`${styles.loginBtn} mt-3 w-100`}>
+              Sign In
+            </Button>
 
-      {errors.non_field_errors && (
-        <Alert variant="warning" className="mt-3">
-          {errors.non_field_errors.map((error, idx) => (
-            <div key={idx}>{error}</div>
-          ))}
-        </Alert>
-      )}
-    </Form>
+            {errors.non_field_errors && (
+              <Alert variant="danger" className="mt-3">
+                {errors.non_field_errors.map((error, idx) => (
+                  <div key={idx}>{error}</div>
+                ))}
+              </Alert>
+            )}
+
+            <p className="text-end small text-muted mt-1">
+              Don't have an account? <a href="/signup">Sign Up</a>
+            </p>
+          </Form>
+          <hr />
+          <p className="text-center mb-0"><i className="fa-solid fa-book-open-reader"></i></p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
