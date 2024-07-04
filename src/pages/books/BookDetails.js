20b92f2a8f933d7764463d82ae8751e91bd64b9e
Lizah Gyllingsten <gyllingstenlizah@gmail.com>
2024-07-04 14:13:42 +0000

    Add signin/signup prompt if not logged in and fixed responsivity issues to book details page

--- src/pages/books/BookDetails.js
+++ src/pages/books/BookDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Container, Row, Col, Accordion, Card, Dropdown } from "react-bootstrap";
+import { Container, Row, Col, Accordion, Card, Dropdown, Image } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import StarRating from "../../components/StarRating";
 import Avatar from "../../components/Avatar";
@@ -10,6 +10,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import styles from "../../styles/BookDetails.module.css";
 import PopularBooks from "../../components/PopularBooks";
 import bgImage from "../../assets/book-details-bg.png"
+import NoCurrentUser from "../../components/NoCurrentUser";
 
 const BookDetails = () => {
   const { id } = useParams();
@@ -79,125 +80,136 @@ const BookDetails = () => {
 
   return (
     <Container>
-      <Row className="d-flex text-center justify-content-center">
-        <Col lg={6} className="d-none d-lg-block">
-          <PopularBooks />
-        </Col>
-        <Col lg={6} className={`d-flex flex-column mb-3 justify-content-between ${styles.mainContainer}`}>
-            <div>
-              <h1>{book.title}</h1>
-              <p>By {book.author}</p>
-              <hr />
-              {book.series && (
-                <p className="mb-0 small">{book.series} #{book.series_number}</p>
-              )}
-              <div className="d-flex justify-content-center">
-                {book.genres && book.genres.map((genre, index) => (
-                  <p key={index} className={`${styles.genreTag} mx-1 p-1`}>#{genre}</p>
-                ))}
+      {!currentUser ? (
+        <NoCurrentUser />
+      ) : (
+        <>
+          <Row className="d-flex text-center justify-content-center">
+            <Col lg={6} className="d-none d-lg-block">
+              <PopularBooks />
+            </Col>
+            <Col lg={6} className={`d-flex flex-column mb-3 justify-content-between ${styles.mainContainer}`}>
+              <div>
+                <h1>{book.title}</h1>
+                <p>By {book.author}</p>
+                <hr />
+                {book.series && (
+                  <p className="mb-0 small">{book.series} #{book.series_number}</p>
+                )}
+                <div className="d-flex justify-content-center">
+                  {book.genres && book.genres.map((genre, index) => (
+                    <p key={index} className={`${styles.genreTag} mx-1 p-1`}>#{genre}</p>
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className="w-100 d-flex justify-content-center">
-              <img className={`mb-0 w-100 ${styles.backImg}`} src={bgImage} alt='stephen king quote' />
-            </div>
-        </Col>
-      </Row>
+              <div className="w-100 d-flex justify-content-center">
+                <img className={`mb-0 w-100 ${styles.backImg}`} src={bgImage} alt='stephen king quote' />
+              </div>
+            </Col>
+          </Row>
 
-      <Row className="my-3">
-        <Col className={styles.plainContainer}>
-          <h4>Description</h4>
-          <p>{book.description}</p>
-        </Col>
-      </Row>
+          <Row className={`${styles.plainContainer} d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between my-3`}>
+            <Col lg={2} md={12} className="text-center">
+              <Image className={styles.coverImage} src={book.cover_image} />
+              <hr className="d-block d-lg-none" />
+            </Col>
+            <Col lg={9} md={12} sm={12} className="me-3">
+              <h4>Description</h4>
+              <p>{book.description}</p>
+            </Col>
+          </Row>
 
-      <Row className={styles.mainContainer}>
-        <Col>
-          {currentUser && (
-            <div className={`${styles.reviewBtn} mt-2 d-flex justify-content-between`}>
-              <h3 className="mb-4">Reviews</h3>
-              {hasUserReviewedBook ? (
-                <span onClick={handleShowReviewModal}>
-                  <i className="fa-solid fa-feather"></i>
-                  <i className="fa-solid fa-user-plus"></i>
-                </span>
-              ) : (
-                <span onClick={handleShowReviewModal} className="text-muted small">
-                  Add <i className="fa-solid fa-feather"></i>
-                </span>
-              )}
-            </div>
-          )}
-          <Accordion>
-            {reviews.length > 0 ? (
-              reviews.map((review, index) => (
-                <Card key={review.id}>
-                  <Accordion.Toggle as={Card.Header} eventKey={index.toString()}>
-                    <Row className="align-items-center">
-                      <Col sm={4}>
-                        <StarRating rating={review.rating} />
-                      </Col>
-                      <Col sm={5}>{review.owner}</Col>
-                      <Col sm={3}>
-                        <p className="text-muted small text-end mb-0">
-                          Reviewed on: <br />
-                          {new Date(review.created_at).toLocaleDateString()}
-                        </p>
-                      </Col>
-                    </Row>
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey={index.toString()}>
-                    <Card.Body>
-                      <Row>
-                        <Col sm={2}>
-                          <Avatar src={review.owner_avatar} height={60} />
-                        </Col>
-                        <Col sm={9}>
-                          <p>{review.comment}</p>
-                        </Col>
-                        <Col sm={1} className="d-flex flex-column align-items-end mb-auto">
-                          {currentUser?.username === review.owner && (
-                            <Dropdown className="mt-auto">
-                              <Dropdown.Toggle
-                                variant="secondary"
-                                id="dropdown-basic"
-                                className="customDropDown"
-                              >
-                                <i className="fa-solid fa-gear"></i>
-                              </Dropdown.Toggle>
-                              <Dropdown.Menu>
-                                <Dropdown.Item onClick={() => handleEditReview(review.id)}>
-                                  Edit
-                                </Dropdown.Item>
-                                <Dropdown.Item onClick={() => handleDeleteReview(review.id)}>
-                                  Delete
-                                </Dropdown.Item>
-                              </Dropdown.Menu>
-                            </Dropdown>
-                          )}
-                        </Col>
-                      </Row>
-                      <Row className="mt-3">
-                        <Col sm={12} className="text-end">
-                          <LikeButton review={review} updateReview={updateReview} />
-                        </Col>
-                      </Row>
-                    </Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              ))
-            ) : (
-              <p>No reviews yet.</p>
-            )}
-          </Accordion>
-        </Col>
-      </Row>
+          <Row className={styles.mainContainer}>
+            <Col>
+              <div className={`${styles.reviewBtn} mt-2 d-flex justify-content-between`}>
+                <h3 className="mb-4">Reviews</h3>
+                {hasUserReviewedBook ? (
+                  <span onClick={handleShowReviewModal}>
+                    <i className="fa-solid fa-feather"></i>
+                    <i className="fa-solid fa-user-plus"></i>
+                  </span>
+                ) : (
+                  <span onClick={handleShowReviewModal} className="text-muted small">
+                    Add <i className="fa-solid fa-feather"></i>
+                  </span>
+                )}
+              </div>
+              <Accordion>
+                {reviews.length > 0 ? (
+                  reviews.map((review, index) => (
+                    <Card key={review.id}>
+                      <Accordion.Toggle as={Card.Header} eventKey={index.toString()}>
+                        <Row className="align-items-center">
+                          <Col sm={4} className="d-none d-sm-block">
+                            <StarRating rating={review.rating} />
+                          </Col>
+                          <Col sm={5} xs={5}>{review.owner}</Col>
+                          <Col sm={3} xs={7}>
+                            <p className="text-muted small text-end mb-0">
+                              Reviewed on: <br />
+                              {new Date(review.created_at).toLocaleDateString()}
+                            </p>
+                          </Col>
+                        </Row>
+                      </Accordion.Toggle>
+                      <Accordion.Collapse eventKey={index.toString()}>
+                        <Card.Body className="pe-0 pb-0">
+                          <Row >
+                            <Col xs={2} className="d-none d-sm-block">
+                              <Avatar src={review.owner_avatar} height={60} />
+                            </Col>
+                            <Col xs={9}>
+                              <p>{review.comment}</p>
+                            </Col>
+                            <Col xs={1} className="d-flex flex-column justify-content-end mb-auto">
+                              {currentUser?.username === review.owner && (
+                                <Dropdown className="mt-auto">
+                                  <Dropdown.Toggle
+                                    variant="secondary"
+                                    id="dropdown-basic"
+                                    className="customDropDown"
+                                  >
+                                    <i className="fa-solid fa-gear"></i>
+                                  </Dropdown.Toggle>
+                                  <Dropdown.Menu>
+                                    <Dropdown.Item onClick={() => handleEditReview(review.id)}>
+                                      Edit
+                                    </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => handleDeleteReview(review.id)}>
+                                      Delete
+                                    </Dropdown.Item>
+                                  </Dropdown.Menu>
+                                </Dropdown>
+                              )}
+                            </Col>
+                          </Row>
+                          <Row className="mt-3">
+                            <Col xs={8} className="text-start d-block d-sm-none">
+                              <StarRating rating={review.rating} />
+                            </Col>
+                            <Col xs={4} sm={12} className="text-end">
+                              <LikeButton review={review} updateReview={updateReview} />
+                            </Col>
+                          </Row>
+                        </Card.Body>
+                      </Accordion.Collapse>
+                    </Card>
+                  ))
+                ) : (
+                  <p>No reviews yet.</p>
+                )}
+              </Accordion>
+            </Col>
+          </Row>
 
-      <CreateReviewForm
-        bookId={book.id}
-        show={showReviewModal}
-        handleClose={handleCloseReviewModal}
-        initialReview={userReview}
-      />
+          <CreateReviewForm
+            bookId={book.id}
+            show={showReviewModal}
+            handleClose={handleCloseReviewModal}
+            initialReview={userReview}
+          />
+        </>
+      )}
     </Container>
   );
 };
