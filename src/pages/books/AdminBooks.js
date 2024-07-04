36db902781ccb7e1a7959f8d654cc3a1099a89b0
Lizah Gyllingsten <gyllingstenlizah@gmail.com>
2024-07-04 16:36:24 +0000

    Add responsiveness to admin book panel, ability to choose smaller table on smaller screens

--- src/pages/books/AdminBooks.js
+++ src/pages/books/AdminBooks.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Table, Button, Row, Col, Form, Container, Alert } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import styles from "../../styles/AdminBooks.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
@@ -9,8 +9,23 @@ const AdminBooks = () => {
   const [books, setBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [viewMode, setViewMode] = useState("large");
   const currentUser = useCurrentUser();
   const history = useHistory();
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setViewMode("large");
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -25,9 +40,10 @@ const AdminBooks = () => {
   }, []);
 
   useEffect(() => {
-    const results = books.filter((book) =>
-      book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      book.author.toLowerCase().includes(searchTerm.toLowerCase())
+    const results = books.filter(
+      (book) =>
+        book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        book.author.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setSearchResults(results);
   }, [searchTerm, books]);
@@ -51,17 +67,35 @@ const AdminBooks = () => {
 
   const isAdmin = currentUser?.is_admin;
 
+  const toggleViewMode = () => {
+    setViewMode(viewMode === "large" ? "small" : "large");
+  };
+
   return (
     <Container className={styles.mainContainer}>
       {isAdmin ? (
         <>
+        <Row className="d-block d-md-none">
+          <Alert variant="warning" className="text-center">
+              Admin panel is designed for larger screens. Please choose to view either the full "large" panel or a streamlined panel suitable for small screens.<br /><br />
+              {viewMode === "large" ? (
+                <>
+                <Button onClick={toggleViewMode} variant="outline-success" className={`${styles.toggleBtn} ms-2`}>
+                Switch to Small View
+                </Button>
+                </>
+              ) : 
+              <>
+                  <Button onClick={toggleViewMode} variant="outline-danger" className={`${styles.toggleBtn} ms-2`}>
+                    Switch to Large View
+                  </Button>
+              </>}
+          </Alert>
+        </Row>
           <Row className="mb-4 justify-content-center">
             <Col md={8}>
               <div className="text-center">
                 <h1>Admin Book Management</h1>
-                <Button as={Link} to="/books" variant="primary" className={`${styles.backBtn} mb-3`}>
-                  Back to Book List
-                </Button>
                 <hr />
                 <p className="small text-muted">
                   Manage your book library from this admin panel.
@@ -80,62 +114,89 @@ const AdminBooks = () => {
           <Table striped bordered hover>
             <thead>
               <tr>
-                <th>Cover</th>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Description</th>
-                <th>Reviews</th>
-                <th>Actions</th>
+                {viewMode === "large" ? (
+                  <>
+                    <th>Cover</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Description</th>
+                    <th>Reviews</th>
+                    <th>Actions</th>
+                  </>
+                ) : (
+                  <>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Actions</th>
+                  </>
+                )}
               </tr>
             </thead>
             <tbody>
               {searchResults.map((book) => (
                 <tr key={book.id}>
-                  <td>{book.cover_image.includes('nocover_dhpojf') ? <>
-                      <i className="fa-regular fa-circle-xmark" style={{color: "red", fontSize: "120%", padding: "0"}}></i>
-                    </> : <>
-                      <i className="fa-regular fa-circle-check" style={{color: "green", fontSize: "120%", padding: "0"}}></i>
-                    </>}
-                  </td>
-                  <td>{book.title}</td>
-                  <td>{book.author}</td>
-                  <td>
-                    <div className={styles.toolTip}>
-                      {book.description.length > 40
-                        ? `${book.description.substring(0, 40)}...`
-                        : book.description}
-                      <span className={styles.toolTiptext}>
-                        {book.description}<br/><br />
-                        {book.genres && book.genres.length > 0 ? (
-                          <>
-                            <strong>Genres:</strong> {book.genres.join(', ')}<br/>
-                          </>
-                        ) : (
-                          <>
-                          <em>No Genre Information</em> <br />
-                          </>
-                        )}
-                        {book.series ? (
-                          <>
-                            <strong>Series:</strong> {book.series} #{book.series_number}<br/>
-                          </>
-                        ) : (
-                          <>
-                          <em>No Series Information</em> <br />
-                          </>
-                        )}
-                      </span>
-                    </div>
-                  </td>
-                  <td>{book.reviews_count}</td>
-                  <td>
-                    <span onClick={() => history.push(`/admin/books/edit/${book.id}`)} className={`${styles.editBtn} me-2`}>
-                      <i className="fa-solid fa-pen-to-square"></i>
-                    </span>
-                    <span className={`${styles.deleteBtn} me-2`} onClick={() => handleDelete(book.id)}>
-                      <i className="fa-solid fa-trash"></i>
-                    </span>
-                  </td>
+                  {viewMode === "large" ? (
+                    <>
+                      <td>{book.cover_image.includes('nocover_dhpojf') ? <>
+                          <i className="fa-regular fa-circle-xmark" style={{color: "red", fontSize: "120%", padding: "0"}}></i>
+                        </> : <>
+                          <i className="fa-regular fa-circle-check" style={{color: "green", fontSize: "120%", padding: "0"}}></i>
+                        </>}
+                      </td>
+                      <td>{book.title}</td>
+                      <td>{book.author}</td>
+                      <td>
+                        <div className={styles.toolTip}>
+                          {book.description.length > 40
+                            ? `${book.description.substring(0, 40)}...`
+                            : book.description}
+                          <span className={styles.toolTiptext}>
+                            {book.description}<br/><br />
+                            {book.genres && book.genres.length > 0 ? (
+                              <>
+                                <strong>Genres:</strong> {book.genres.join(', ')}<br/>
+                              </>
+                            ) : (
+                              <>
+                              <em>No Genre Information</em> <br />
+                              </>
+                            )}
+                            {book.series ? (
+                              <>
+                                <strong>Series:</strong> {book.series} #{book.series_number}<br/>
+                              </>
+                            ) : (
+                              <>
+                              <em>No Series Information</em> <br />
+                              </>
+                            )}
+                          </span>
+                        </div>
+                      </td>
+                      <td>{book.reviews_count}</td>
+                      <td>
+                        <span onClick={() => history.push(`/admin/books/edit/${book.id}`)} className={`${styles.editBtn} me-2`}>
+                          <i className="fa-solid fa-pen-to-square"></i>
+                        </span>
+                        <span className={`${styles.deleteBtn} me-2`} onClick={() => handleDelete(book.id)}>
+                          <i className="fa-solid fa-trash"></i>
+                        </span>
+                      </td>
+                    </>
+                  ) : (
+                    <>
+                      <td>{book.title}</td>
+                      <td>{book.author}</td>
+                      <td>
+                        <span onClick={() => history.push(`/admin/books/edit/${book.id}`)} className={`${styles.editBtn} me-2`}>
+                          <i className="fa-solid fa-pen-to-square"></i>
+                        </span>
+                        <span className={`${styles.deleteBtn} me-2`} onClick={() => handleDelete(book.id)}>
+                          <i className="fa-solid fa-trash"></i>
+                        </span>
+                      </td>
+                    </>
+                  )}
                 </tr>
               ))}
             </tbody>
